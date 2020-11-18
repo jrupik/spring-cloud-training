63d860d70020e3a2c04389f68fb78b47532da9ff
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2020-11-18 12:15:03 +0100

    payments api

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-./mvnw clean package -DskipTest=true
+./mvnw clean package -DskipTests=true
+docker rmi --force $(docker images | grep 'training')
 docker build -t training/configuration-server configuration-server
+docker build -t training/discovery-server discovery-server
